8447b53659d9f89bf72950bc5af184ce2cc647bc
Jordan Hudgens <jordan@wow.com>
2014-10-11 14:47:43 -0500

    added listing model and view iterator

--- app.js
+++ app.js
@@ -26,5 +26,13 @@ angular.module('mongoList', ['ui.router'])
     .controller('MainCtrl', [
         '$scope',
         function($scope){
-            $scope.mySomething = 'Hello world!';
+            $scope.listings = [
+                {title: 'listing 1', likes: 2},
+                {title: 'listing 2', likes: 1},
+                {title: 'listing 3', likes: 3},
+                {title: 'listing 4', likes: 9},
+                {title: 'listing 5', likes: 22},
+                {title: 'listing 6', likes: 0},
+                {title: 'listing 7', likes: 1},
+            ];
         }]);
