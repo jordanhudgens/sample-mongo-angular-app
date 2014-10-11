744d8d52a9c278b779b7ece3e1246c7e4c23f61a
Jordan Hudgens <jordan@wow.com>
2014-10-11 14:28:27 -0500

    incorporated routing

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-angular.module('mongoList', [])
+angular.module('mongoList', ['ui.router'])
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                })
+                .state('listings', {
+                    url: '/listings/{id}',
+                    templateUrl: '/listings.html',
+                    controller: 'ListingsCtrl'
+                });
+
+            $urlRouterProvider.otherwise('home');
+        }])
+    .controller('ListingsCtrl', [
+        '$scope',
+        function($scope){
+            $scope.mySomething = 'Hello world!';
+        }])
     .controller('MainCtrl', [
         '$scope',
         function($scope){
