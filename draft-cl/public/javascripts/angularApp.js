f2c88941fbc162e3147fd75a76873e22b464af32
Jordan Hudgens <jordan@wow.com>
2014-10-15 06:04:07 -0500

    integrated connection with questions and likes

--- draft-cl/public/javascripts/angularApp.js
+++ draft-cl/public/javascripts/angularApp.js
@@ -19,22 +19,52 @@ angular.module('mongoList', ['ui.router', 'ngResource'])
             $urlRouterProvider.otherwise('home');
         }])
     .factory('Listing',
-        function($resource){
-            var res = $resource('/listings');
+        function($resource, $q){
+            var res = $resource('/listings/:id', {id: '@id'});
+            var resQuestions = $resource('/listings/:listingId/questions/', {listingId: '@listingId'});
+            var resLikes = $resource('/listings/:listingId/like', {listingId: '@listingId'});
             return {
                 create: function(attrs) {
                     var listing = new res(attrs);
                     listing.$save();
                 },
+                get: function(id) {
+                    var deferred = $q.defer();
+                    res.get({id: id}, function(listing) {
+                        deferred.resolve(listing);
+                    });
+                    return deferred.promise;
+                },
                 all: function() {
                     return res.query();
+                },
+                addQuestion: function(listingId, attrs) {
+                    attrs.listingId = listingId;
+                    var question = new resQuestions(attrs);
+                    question.$save();
+                },
+                addLike: function(listingId) {
+                    var like = new resLikes({listingId: listingId});
+                    like.$save();
                 }
             };
         })
     .controller('ListingsCtrl', [
-        '$scope',
-        function($scope){
-            $scope.mySomething = 'Hello world!';
+        '$scope', '$stateParams', 'Listing',
+        function($scope, $stateParams, Listing){
+            Listing.get($stateParams.id).then(function(listing) {
+                $scope.listing = listing;
+            });
+            $scope.addQuestion = function () {
+                var question = {body: $scope.question, customer: $scope.name};
+                Listing.addQuestion($scope.listing._id, question);
+                $scope.listing.questions.push(question);
+                $scope.question = $scope.name = '';
+            };
+            $scope.addLike = function () {
+                Listing.addLike($scope.listing._id);
+                $scope.listing.likes += 1;
+            }
         }])
     .controller('MainCtrl', [
         '$scope',
--- draft-cl/routes/index.js
+++ draft-cl/routes/index.js
@@ -21,6 +21,26 @@ router.get('/listings', function(req, res, next) {
     });
 });
 
+// Preload post objects on routes with ':listing'
+router.param('listing', function(req, res, next, id) {
+    var query = Listing.findById(id);
+
+    query.exec(function (err, listing){
+        if (err) { return next(err); }
+        if (!listing) { return next(new Error("can't find listing")); }
+
+        req.listing = listing;
+        return next();
+    });
+});
+
+// return a listing
+router.get('/listings/:listing', function(req, res, next) {
+    req.listing.populate('questions', function(err, listing) {
+        res.json(listing);
+    });
+});
+
 router.post('/listings', function(req, res, next) {
     var listing = new Listing(req.body);
 
@@ -29,6 +49,40 @@ router.post('/listings', function(req, res, next) {
 
         res.json(listing);
     });
+});
+
+router.post("/listings/:listing/questions", function(req, res, next) {
+    var question = new Question(req.body);
+    question.listing = res.listing;
+
+    question.save(function(err, question){
+        if(err){ return next(err); }
+
+        req.listing.questions.push(question);
+        req.listing.save(function(err, listing) {
+            if(err){ return next(err); }
+
+            res.json(question);
+        });
+    });
+});
+
+// For likes
+
+router.post("/listings/:listing/like", function(req, res, next) {
+   var update = { $inc: { likes: 1 }};
+   Listing.update({_id: req.listing._id}, update).exec();
+   res.json({});
+
+    /*
+   req.listing.likes += 1;
+
+    req.listing.save(function(err, listing) {
+        if(err){ return next(err); }
+
+        res.json(listing);
+    });
+    */
 });
 
 module.exports = router;
